a59285efda00102bb0ea83eb6c1f8bfea7a20116
Shubham <mb.shbuhamshah@gmail.com>
2024-11-14 08:18:50 +0530

    changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 8000;
 
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://64.227.143.155:3000',
+    origin: [process.env.CORS_ORIGIN, 'http://registration.buniyaadhry.com'],
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true
 }));
